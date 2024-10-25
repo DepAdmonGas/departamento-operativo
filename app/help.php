126cbc235316615e5939bac0f9155c2d4d9a781b
brayanAdmonGas <brayan.ac@admongas.com.mx>
2024-10-25 13:01:04 -0600

    cache

--- app/help.php
+++ app/help.php
@@ -1,4 +1,9 @@
 <?php
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT"); // Fecha pasada
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // Fecha de modificación actual
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 include_once 'lib/jwt/vendor/autoload.php';
 include_once "config/inc.configuracion.php";
 include_once "config/ConfiguracionSesiones.php";
